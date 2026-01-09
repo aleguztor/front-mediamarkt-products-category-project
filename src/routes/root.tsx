ec5d64e9fa5b75d55a286b24322c65f65d0cea25
aleguztor <kihacaoficial@gmail.com>
2026-01-09 15:27:04 +0100

    Add Paths to root

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,8 +1,6 @@
 import { RouteObject } from 'react-router-dom';
 import MainLayout from '@/layouts/MainLayout';
-// import { DashboardPage } from '@/pages/Dashboard';
-// import { HomePage } from '@/pages/Home';
-// import { LoginPage } from '@/pages/Login';
+import { CategoriesPage, ProductsPage } from '@/pages';
 import { PATHS } from './paths';
 
 export const routes: RouteObject[] = [
@@ -13,16 +11,20 @@ export const routes: RouteObject[] = [
   {
     path: PATHS.ROOT,
     element: <MainLayout />, // Aquí van el Menú y Sidebar de PrimeReact
-    // children: [
-    //   {
-    //     index: true,
-    //     element: null, // <HomePage />,
-    //   },
-    //   // {
-    //   //   path: PATHS.DASHBOARD.ROOT,
-    //   //   element: <DashboardPage />,
-    //   // },
-    //   // Añadir más páginas aquí es muy sencillo
-    // ],
+    children: [
+      {
+        index: true,
+        element: PATHS.DASHBOARD.ROOT, // <HomePage />,
+      },
+      {
+        path: PATHS.DASHBOARD.PRODUCTS,
+        element: <ProductsPage />,
+      },
+      {
+        path: PATHS.DASHBOARD.CATEGORIES,
+        element: <CategoriesPage />,
+      },
+      // Añadir más páginas aquí es muy sencillo
+    ],
   },
 ];
