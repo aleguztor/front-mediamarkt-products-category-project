fc62d6fe9eb47eba752150c3529795cad58fc0bb
aleguztor <kihacaoficial@gmail.com>
2026-01-10 15:44:50 +0100

    refactor: :recycle: Add folder for productPage and Add css

---
+++ src/pages/ProductPage/ProductsPage.tsx
@@ -0,0 +1,7 @@
+import styles from './productPage.module.css';
+
+const ProductPage = () => {
+  return <div className={styles.productsPage}></div>;
+};
+
+export default ProductPage;
--- src/pages/ProductsPage.tsx
+++
@@ -1,7 +0,0 @@
-import { PropsWithChildren } from 'react';
-
-const ProductPage = ({ children }: PropsWithChildren) => {
-  return <div className="products-page">{children}</div>;
-};
-
-export default ProductPage;
