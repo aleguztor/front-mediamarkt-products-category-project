9e6c029fe6e007cfdec5414e059659a8c62aef97
aleguztor <kihacaoficial@gmail.com>
2026-01-11 18:07:25 +0100

    feat: :sparkles: Add category set dropdown

--- src/features/Categories/hooks/useCategoryAction.ts
+++ src/features/Categories/hooks/useCategoryAction.ts
@@ -1,19 +1,33 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useQuery } from '@tanstack/react-query';
+import { RootState } from '@/store';
 import { GetCategoriesUseCase } from '../application/GetCategoriesUseCase';
 import { CategoryRemoteRepository } from '../data/CategoryRemoteRepository';
+import { setCategories } from '../store/categorySlice';
 
 const repo = new CategoryRemoteRepository();
 const getCategoriesUC = new GetCategoriesUseCase(repo);
 
 export const useCategoryActions = () => {
-  const usersQuery = useQuery({
+  const dispatch = useDispatch();
+
+  const categoriesFromRedux = useSelector((state: RootState) => state.categories.items);
+
+  const { data, isLoading } = useQuery({
     queryKey: ['categories'],
     queryFn: () => getCategoriesUC.execute(),
+    staleTime: Infinity,
   });
 
+  useEffect(() => {
+    if (data) {
+      dispatch(setCategories(data));
+    }
+  }, [data, dispatch]);
+
   return {
-    categories: usersQuery.data ?? [],
-    isLoading: usersQuery.isLoading,
-    isError: usersQuery.isError,
+    categories: categoriesFromRedux,
+    isLoading: isLoading && categoriesFromRedux.length === 0,
   };
 };
