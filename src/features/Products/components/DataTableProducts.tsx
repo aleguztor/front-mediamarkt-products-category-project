1910f987e61fcbd29e5c082ac11a3080a05ff8c2
aleguztor <kihacaoficial@gmail.com>
2026-01-12 13:56:22 +0100

    feat: :sparkles: Complete filter find, clean button and bit refactor

--- src/features/Products/components/DataTableProducts.tsx
+++ src/features/Products/components/DataTableProducts.tsx
@@ -1,6 +1,12 @@
+import { useMemo, useRef, useState } from 'react';
+import { FilterMatchMode } from 'primereact/api';
 import { Button } from 'primereact/button';
+import { ButtonGroup } from 'primereact/buttongroup';
 import { Column } from 'primereact/column';
-import { DataTable } from 'primereact/datatable';
+import { DataTable, DataTableFilterMeta } from 'primereact/datatable';
+import { IconField } from 'primereact/iconfield';
+import { InputIcon } from 'primereact/inputicon';
+import { InputText } from 'primereact/inputtext';
 import { Product } from '@/core/domain/Product';
 import styles from './product.module.css';
 
@@ -25,9 +31,50 @@ const DataTableProducts = ({
   setIsEditingProduct,
   setIsCreatingNewProduct,
 }: DataTableProductsProps) => {
+  const [globalFilterValue, setGlobalFilterValue] = useState<string>('');
+  const dt = useRef<DataTable<Product[]>>(null);
+  const initialFilters: DataTableFilterMeta = {
+    global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+    name: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+    'category.name': { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+    price: { value: null, matchMode: FilterMatchMode.EQUALS },
+  };
+  const [filters, setFilters] = useState(initialFilters);
+
+  const clearFilters = () => {
+    setFilters(initialFilters);
+    setGlobalFilterValue('');
+  };
+  const onGlobalFilterChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    let _filters = { ...filters };
+
+    // Actualizamos el valor en el objeto de filtros de PrimeReact
+    (_filters['global'] as any).value = value;
+
+    setFilters(_filters);
+    setGlobalFilterValue(value);
+  };
+  const hasFilters = useMemo(() => {
+    return Object.values(filters).some((filter) => {
+      if (!filter) return false;
+
+      // Si es un array de filtros (filtros avanzados)
+      if (Array.isArray(filter)) {
+        return filter.some((f) => f.value !== null && f.value !== '');
+      }
+
+      // Si es un filtro simple
+      return (filter as any).value !== null && (filter as any).value !== '';
+    });
+  }, [filters]);
+
   return (
     <DataTable
+      ref={dt}
+      filters={filters}
       className={styles.table}
+      onFilter={(e) => setFilters(e.filters)}
       resizableColumns
       stripedRows
       virtualScrollerOptions={{ itemSize: 25 }}
@@ -37,14 +84,34 @@ const DataTableProducts = ({
       value={products}
       header={
         <div className={styles.header}>
-          <h2>Productos</h2>
-          <Button
-            onClick={() => setIsCreatingNewProduct(true)}
-            icon="pi pi-plus"
-            severity="info"
-            size="small"
-            label={window.innerWidth > 600 ? 'Agregar Producto' : ''}
-          />
+          <IconField style={{ flex: 1 }} iconPosition="left">
+            <InputIcon className="pi pi-search" />
+            <InputText
+              style={{ width: '100%' }}
+              value={globalFilterValue}
+              onChange={onGlobalFilterChange}
+              placeholder="Buscar producto..."
+              className="p-inputtext-sm"
+            />
+          </IconField>
+          <ButtonGroup>
+            <Button
+              type="button"
+              icon="pi pi-filter-slash"
+              label="Limpiar"
+              disabled={!hasFilters}
+              onClick={clearFilters}
+              size="small"
+              severity="contrast"
+            />
+            <Button
+              onClick={() => setIsCreatingNewProduct(true)}
+              icon="pi pi-plus"
+              severity="info"
+              size="small"
+              label={window.innerWidth > 600 ? 'Agregar Producto' : ''}
+            />
+          </ButtonGroup>
         </div>
       }
       loading={isLoading}
