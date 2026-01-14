7fc61f211d29cd1aecf8677a2d08b204cedf5e95
Alejandro Guzmán Torres <alejandrogtdev@gmail.com>
2026-01-14 16:42:34 +0100

    feat: :sparkles: Config Api calls with Filters

---
+++ src/core/models/ProductsFilterRequest.ts
@@ -0,0 +1,9 @@
+import { PagingParams } from './Common/PagingParams';
+
+export interface ProductsFilterRequest extends PagingParams {
+  generalSearch?: string | null;
+  minPrice?: number | null;
+  maxPrice?: number | null;
+  name?: string | null;
+  category?: string[] | null;
+}
